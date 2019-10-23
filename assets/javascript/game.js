312da674358914508208a7bc0b8a30ba1209c039
Mary Frances <mfskibiel@gmail.com>
2019-10-23 12:51:31 -0400

    completed html structure

---
+++ assets/javascript/game.js
@@ -0,0 +1,41 @@
+//global variables
+console.log("please work");
+
+var crystal = {
+    blue:
+    {
+        name: "blue", value: 0
+    },
+    red:
+    {
+        name: "red", value: 0
+    },
+    purple:
+    {
+        name: "purple", value: 0
+    },
+    green:
+    {
+        name: "green", value: 0
+    },
+};
+
+var currentscore = 0;
+var targetscore = 0;
+var wins = 0;
+var losses = 0;
+
+
+$("#blue").on("click", function () {
+    alert("test");
+});
+$("#red").on("click", function () {
+    alert("test");
+});
+$("#purple").on("click", function () {
+    alert("test");
+});
+$("#green").on("click", function () {
+    alert("test");
+});
+
